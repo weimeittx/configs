6b53bab487063295b3e32fe2c0dda9bff98a3c12
weimeittx <422450455@qq.com>
2019-07-21 22:37:12 +0800

    Update run.js

--- world/run.js
+++ world/run.js
@@ -1,17 +1,2 @@
-var npcs = com.hz.main.GameWorld.getNPCList();
-for(var npcIndex  in npcs){
-	var npc = npcs[npcIndex];
-	cmd.alert("|"+npc.getName()+"|")
-	if(npc.getName().equals("丛林守卫")){
-		cmd.alert(java.lang.String.valueOf(npc.missions));
-		cmd.alert(java.lang.String.valueOf(npc.missions.size()));
-		if(npc.missions.size() > 0){
-			for(var mIndex in npc.missions){
-				var m = npc.missions[mIndex];
-				
-				cmd.alert(m.name);
-				cmd.alert(m.getNameInfo());
-			}
-		}
-	}
-}
+cmd.acceptMission("丛林守卫","消灭腐化的生物");
+cmd.alert("脚本结束!")
