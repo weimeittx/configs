a4679ab53f243947e01b9212be678fb9faebff78
weimeittx <422450455@qq.com>
2019-07-21 20:03:37 +0800

    Update run.js

--- world/run.js
+++ world/run.js
@@ -1,3 +1,3 @@
-while(true){
-  java.lang.System.out.println("11111");
-      }
+com.hz.main.GameWorld.myPlayer.px = (22*24);
+com.hz.main.GameWorld.myPlayer.py = (22*24);
+com.hz.main.MsgHandler.syncMoveTo();
