ea4461f844156b074f8346ce9cdaeba41de1e642
weimeittx <422450455@qq.com>
2019-07-21 19:56:36 +0800

    Update run.js

--- world/run.js
+++ world/run.js
@@ -1,3 +1,2 @@
-com.hz.main.GameWorld.myPlayer.px = 22*24;
-com.hz.main.GameWorld.myPlayer.py = 22*24;
-com.hz.main.MsgHandler.syncMoveTo();
+com.hz.main.GameWorld.myPlayer.px = (22*24);
+com.hz.main.GameWorld.myPlayer.py = (22*24);
