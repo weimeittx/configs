f1f037239bd4b119be7db068cdf30af2f82c7e70
weimeittx <422450455@qq.com>
2019-07-21 22:19:08 +0800

    Update run.js

--- world/run.js
+++ world/run.js
@@ -2,7 +2,7 @@ var npcs = com.hz.main.GameWorld.getNPCList()
 for(var npcIndex  in npcs){
 	var npc = npcs[npcIndex];
 	cmd.alert("|"+npc.getName()+"|")
-	if(npc.getName() == "丛林守卫"){
+	if(npc.getName().equals("丛林守卫")){
 		cmd.alert(npc.missions.size()+"")
 		if(npc.missions.size() > 0){
 			for(var mIndex in npc.missions){
