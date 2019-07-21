61a0354538e293b582146513c1a7502f2f1235dd
weimeittx <422450455@qq.com>
2019-07-21 21:40:28 +0800

    Update run.js

--- world/run.js
+++ world/run.js
@@ -1,10 +1,1 @@
-var f = false;
-while (true) {
-    java.lang.Thread.sleep(1000);
-    if (f) {
-        com.hz.main.GameWorld.doJumpMap(900,2,36);
-    } else {
-        com.hz.main.GameWorld.doJumpMap(901,27,3);
-    }
-    f = !f;
-}
+cmd.alert("测试消息!");
